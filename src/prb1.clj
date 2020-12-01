bb8db9bd8ab14f52a4352f5791ed81cf90dbcddd
Josh Reighley <josh@reighley.net>
2020-12-01 00:02:59 -0800

    Added comments

--- src/prb1.clj
+++ src/prb1.clj
@@ -6,22 +6,28 @@
                (map #(Integer/parseInt %))
                (sort)))  ;; this makes it a little too easy had to comment it out because first answer was correct.
 
-(defn find-pair [n-list target-n]
+(defn find-pair
+  "Given a list of numbers and a target sum, find a pair that sum to target"
+  [n-list target-n]
   (let  [subtracted-data (set (map #(- target-n %) n-list))]
-    (filter subtracted-data n-list)))
+    (filter subtracted-data n-list)))  ;; sets work as a predicate - so find the complements that exist in the original data.
 
 (comment (find-pair data 2020))
 ; (527 1493)
 
 (def answer-1 (reduce  * (find-pair data 2020))) ;786811
 
-(defn find-triple [n-list target-n]
+(defn find-triple
+  "given a list of number and a target sum, find 3 that sum to target"
+  [n-list target-n]
   (let [first-n (first n-list)
-        double-target (- target-n first-n)
+        double-target (- target-n first-n)  ;
         pairs (find-pair (rest n-list) double-target)]
-    (if (= ( count pairs) 2)
-      (conj pairs first-n)
-      (recur (rest n-list) target-n))))  ; (1111 289 620)
+    (if (= ( count pairs) 2)  ;if we find a pair that sum to our target - our first number
+      (conj pairs first-n) ;;return the first number and pairs
+      (recur (rest n-list) target-n))))    ;; else give up on the first number and try again with the rest of the list.
+
+; (1111 289 620)
 
 (def answer-2 (reduce * (find-triple data 2020))) ;;199068980
 
