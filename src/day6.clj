b68e27740746eb0e8ca49afc984011037e1e816f
Josh Reighley <josh@reighley.net>
2020-12-05 21:41:03 -0800

    Completed day 6

---
+++ src/day6.clj
@@ -0,0 +1,25 @@
+(ns day6
+  (:require [util :refer [import-lines]]))
+
+(def data (->> (import-lines "resources/input6")
+               (partition-by empty?)))
+
+(defn find-answer-1 []
+  (->> data
+    (map #(apply str %))
+    (remove empty?)
+    (map set)
+    (map count)
+    (reduce +))) ; 6310 correct
+
+(defn unanimous? [group]
+  (->> (map set  group)
+    (reduce clojure.set/intersection)
+    count))
+
+(defn find-answer-2 []
+  (->> data
+       (remove empty?)
+       (map unanimous?)
+       (reduce +)))  ;3193 correct
+
